6431b03c22965f404f53a6dd95494ed856407395
mariano.palliser <mariano.palliser@sm2baleares.es>
2017-09-05 09:48:20 +0200

    hall of ducks ok

--- duckyTest/src/app/honor-ducks/honor-list/honor-list.component.ts
+++ duckyTest/src/app/honor-ducks/honor-list/honor-list.component.ts
@@ -8,19 +8,27 @@ import {Duck} from '../../duck.model';
 })
 export class HonorListComponent implements OnInit {
   @Output() honorDuckWasSelected = new EventEmitter<Duck>();
+  loremIpsum = 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha ' +
+    'sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. ' +
+    'persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera ' +
+    'que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó ' +
+    'como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue ' +
+    'popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de ' +
+    'Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el ' +
+    'cual incluye versiones de Lorem Ipsum.';
   honorDucks: Duck[] = [
     new Duck('David Gelpi',
       'DUCK MASTER,Deivid, el Gelpi, aquel que nos grita, Odin',
       'https://i.gyazo.com/85ddf189123f5d438bd10f11f73fcab7.png',
-      'asdf'),
+      this.loremIpsum),
     new Duck('Miquel Urbina',
       'UrbinaPro, El que recomienda formatear, Mr patinete,xX_SkateMaster360_Xx',
-      'https://files.slack.com/files-pri/T2F7TQ7AP-F6X16HWSG/whatsapp_image_2017-02-24_at_17.10.24.jpeg',
-      'asdf'),
+      'https://i.gyazo.com/a2ef3e44b7d9b3ee12ddb03bbe84ae6e.png',
+      this.loremIpsum),
     new Duck('Alex Espinola',
       'Espinola, Juanolas, Archienemigo de Joselito, Eye candy de pili',
       'https://i.gyazo.com/20ef98b18d8954c46ff9522776f9f414.png',
-      'asdf')
+      this.loremIpsum)
     ]
   constructor() { }
 
